657273b42f9e627599356bc385194ed05adf39bb
Uttam Soren <uttamsoren886@gmail.com>
2023-07-26 12:45:25 +0530

    Added the companies Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Companies from "./components/companies/companies";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Hero/> 
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
     </div>
-    
-    
   );
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,8 +19,12 @@ function Hero() {
             </h1>
           </div>
           <div className="flexColStart hero-desc">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easilty
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
 
           <div className="flexCenter search-bar">
@@ -30,12 +34,26 @@ function Hero() {
           </div>
 
           <div className="flexCenter stats">
-            <div className="flexColStart stat">
+            <div className="flexColCenter stat">
               <span>
-                <CountUp start={8000} end={9000} duration={4} />
+                <CountUp start={8800} end={9000} duration={4} />
+                <span>+</span>
               </span>
-              <span>Premium Products</span>
-              <span></span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColCenter  stat">
+              <span>
+                <CountUp start={1650} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColCenter  stat">
+              <span>
+                <CountUp end={28} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Awards Winning</span>
             </div>
           </div>
         </div>
